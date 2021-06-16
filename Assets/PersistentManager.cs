d0be077648a0c64a5a6915a0cc718ae497cbd2de
Fikri Karadereli <fikrikaradereli@hotmail.com>
2021-06-16 09:53:12 +0300

    makes the singleton class generic.

--- Assets/PersistentManager.cs
+++ Assets/PersistentManager.cs
@@ -1,23 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PersistentManager : MonoBehaviour
+public class PersistentManager : Singleton<PersistentManager>
 {
-    public static PersistentManager Instance { get; private set; }
-
     public int value;
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 }
